61ef4a66ae0ed12698ea6352d10fa85d05eba709
jacklover22 <109988100+jacklover22@users.noreply.github.com>
2022-08-16 23:15:23 +0300

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,17 @@
+var messages = [
+    'this page is cRINGE!!!!',
+    'this website is so nice but this page doesnt exist',
+    'totally not the page you are looking for',
+    'no jackva is not here',
+    'qwq this  pAGE IS NOT existing',
+    'idkw hat are you lookin for',
+    'this page doesnt exist becAUSE JACK ANIMATES RUINED IT!!!',
+    'cool message subscribe to jackva gaming btw',
+    'wrong page idiot XD XD',
+    'subscribe to corina the wolf guys really cool person guys',
+    'LMA..... im not happy today',
+]
+function newMessage() {
+        var randomNumber = Math.floor(Math.random() * (messages.length));
+        document.getElementById('messsageDisplay').innerHTML = messages[randomNumber];
+}
